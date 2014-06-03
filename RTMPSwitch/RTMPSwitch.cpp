1f7dc53467b252b087073b2ca00f743c774dfbbf
Callum McLean <calmcl1@aol.com>
2014-06-03 16:12:24 +0100

    Live555 attempt

--- RTMPSwitch/RTMPSwitch.cpp
+++ RTMPSwitch/RTMPSwitch.cpp
@@ -4,6 +4,9 @@
 extern "C"{
 #include "lib\librtmp\rtmp.h"
 }
+#include "lib\live\liveMedia\include\liveMedia.hh"
+#include "lib\live\BasicUsageEnvironment\include\BasicUsageEnvironment.hh"
+
 
 #include <iostream>
 
@@ -15,11 +18,21 @@ int main(int argc, char* argv[])
 	cout << "Starting RTMPSwitch" << endl;
 	cout << RTMP_LibVersion << endl;
 
-	RTMP * rtmp_handle = RTMP_Alloc();
+	BasicTaskScheduler* sch = BasicTaskScheduler::createNew();
+	BasicUsageEnvironment* env = BasicUsageEnvironment::createNew(*sch);
+
+	const char url[] = "http://example.com";
+
+	RTSPClient* rtspclient = RTSPClient::createNew(&env, url);
+
+	char eventLoopWatchVariable = 0;
+	sch->doEventLoop(&eventLoopWatchVariable);
+
+	/*RTMP * rtmp_handle = RTMP_Alloc();
 	RTMP_Init(rtmp_handle);
 	char url[] = "http://www.myurl.com";
 	RTMP_SetupURL(rtmp_handle, url);
 	RTMPPacket* rp;
-	RTMP_Connect(rtmp_handle, rp);
+	RTMP_Connect(rtmp_handle, rp);*/
 
 }
