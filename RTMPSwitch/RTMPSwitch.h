49139441bd6333a13611433369df7ae83b7f2a0f
Callum McLean <calmcl1@aol.com>
2014-06-05 15:31:19 +0100

    Added makefile

---
+++ RTMPSwitch/RTMPSwitch.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   RTMPSwitch.h
+ * Author: callum
+ *
+ * Created on 04 June 2014, 16:51
+ */
+
+#ifndef RTMPSWITCH_H
+#define	RTMPSWITCH_H
+
+int main(int argc, char* argv[]);
+
+#endif	/* RTMPSWITCH_H */
+
