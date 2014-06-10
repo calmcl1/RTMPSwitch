19a2aee8d19312cfd2584e4f11e1d9062cd3fef9
Callum McLean <calmcl1@aol.com>
2014-06-10 17:39:49 +0100

    Got ogg-encoded audio stream from stdout working in ffplay

--- RTMPSwitch/RTMPSwitch.cpp
+++ RTMPSwitch/RTMPSwitch.cpp
@@ -10,14 +10,14 @@ extern "C" {
 #include "include/librtmp/rtmp.h"
 #include <gstreamer-1.0/gst/gst.h>
 }
-#include <glog/logging.h>
+
 #include "RTMPSwitch.h"
 
 using namespace std;
 
 static GMainLoop * loop;
 GstElement *pipeline, *audiobin;
-GstElement *decoder, *conv, *vorbisenc, *sink;
+GstElement *decoder, *conv, *vorbisenc, *oggmux, *sink;
 
 gboolean bus_callback(GstBus *bus, GstMessage *message, gpointer data) {
     //cout << "Got message: " << GST_MESSAGE_TYPE_NAME(message) << endl;
@@ -34,23 +34,27 @@ gboolean bus_callback(GstBus *bus, GstMessage *message, gpointer data) {
             cout << "Error: " << err->message << endl;
             g_error_free(err);
             g_free(debug);
-            
+
             g_main_loop_quit(loop);
     }
 }
 
 void on_pad_added(GstElement *decoder, GstPad *pad, gpointer data) {
 
+    // When uridecode finds a stream, it adds a pad and calls this
+
     GstCaps *caps;
     GstStructure *str;
     GstPad *audiopad;
 
+    // Make sure the audiobin isn't already linked
     audiopad = gst_element_get_static_pad(audiobin, "sink");
     if (GST_PAD_IS_LINKED(audiopad)) {
         g_object_unref(audiopad);
         return;
     }
 
+    // Find out what kind of pad is available
     caps = gst_pad_query_caps(pad, NULL);
     str = gst_caps_get_structure(caps, 0);
     if (!g_strrstr(gst_structure_get_name(str), "audio")) {
@@ -60,6 +64,7 @@ void on_pad_added(GstElement *decoder, GstPad *pad, gpointer data) {
     }
 
     gst_caps_unref(caps);
+    // Link the new pad to the audiobin
     gst_pad_link(pad, audiopad);
     g_object_unref(audiopad);
 
@@ -67,8 +72,6 @@ void on_pad_added(GstElement *decoder, GstPad *pad, gpointer data) {
 
 int main(int argc, char* argv[]) {
 
-    google::InitGoogleLogging(argv[0]);
-    
     // Set program version
     const char version[] = "0.1.0";
 
@@ -80,36 +83,50 @@ int main(int argc, char* argv[]) {
     gst_version(&gst_major, &gst_minor, &gst_micro, &gst_nano);
     loop = g_main_loop_new(NULL, FALSE);
 
-    // Rock and roll!
-    printf("Starting RTMPSwitch v%s (linked against Gstreamer %d.%d.%d)\n",
-            version, gst_major, gst_minor, gst_micro);
+    //printf("Starting RTMPSwitch v%s (linked against Gstreamer %d.%d.%d)\n",
+    //       version, gst_major, gst_minor, gst_micro);
 
-
+    // Create the pipeline, attach a bus
     pipeline = gst_pipeline_new("my-pipeline");
     GstBus *bus = gst_pipeline_get_bus(GST_PIPELINE(pipeline));
     guint bus_watch_id = gst_bus_add_watch(bus, bus_callback, NULL);
     gst_object_unref(bus);
 
+    /*                       PIPELINE:
+     * 
+     *                   |------------------- audiobin ------------------------------|
+     * -------------     ----------------    -------------    ----------    ----------
+     * | uridecode | ->  | audioconvert | -> | vorbisenc | -> | oggmux | -> | fdsink |
+     * -------------     ----------------    -------------    ----------    ----------
+     *               \   ----------------    -------------  >
+     *                >  | videoconvert | -> | theoraenc | /
+     *                   ----------------    -------------
+     * 
+     */
+
+    // Create the first half of the pipeline
     decoder = gst_element_factory_make("uridecodebin", "uridec");
     g_object_set(G_OBJECT(decoder), "uri", "http://192.168.0.102:8080/test.ogg",
             NULL);
+
+    // uridecode adds a pad for each stream found
     g_signal_connect(decoder, "pad-added", G_CALLBACK(on_pad_added), NULL);
 
-
-    //sink = gst_element_factory_make("fdsink", "sink");
-
+    // Now create the audio-playing part of the pipeline
     audiobin = gst_bin_new("audiobin");
     conv = gst_element_factory_make("audioconvert", "aconv");
-    GstPad * audiopad = gst_element_get_static_pad(conv, "sink");
     vorbisenc = gst_element_factory_make("vorbisenc", "enc");
-    sink = gst_element_factory_make("filesink", "sink");
-    g_object_set(G_OBJECT(sink), "location", "test.ogg", NULL);
-    //g_object_set(G_OBJECT(sink), "dump", TRUE, NULL);
-    gst_bin_add_many(GST_BIN(audiobin), conv, vorbisenc, sink, NULL);
-    //gst_bin_add_many(GST_BIN(audiobin), conv, sink, NULL);
-    gst_element_link_many(conv, vorbisenc, sink, NULL);
-    //gst_element_link_many(conv,sink,NULL);
+    oggmux = gst_element_factory_make("oggmux", "mux");
+    sink = gst_element_factory_make("fdsink", "sink");
 
+    // The next line is so that we can grab the sink of audioconvert
+    // and attach it to a ghost pad to use for the sink of audiobin
+    GstPad * audiopad = gst_element_get_static_pad(conv, "sink");
+
+    gst_bin_add_many(GST_BIN(audiobin), conv, vorbisenc, oggmux, sink, NULL);
+    gst_element_link_many(conv, vorbisenc, oggmux, sink, NULL);
+
+    // Create the audiobin sink so we can attach it
     gst_element_add_pad(audiobin, gst_ghost_pad_new("sink", audiopad));
 
     gst_bin_add_many(GST_BIN(pipeline), decoder, audiobin, NULL);
@@ -121,6 +138,7 @@ int main(int argc, char* argv[]) {
     //GstElement rtp1 = create_rtp_receiver("12345");
     //rtp_receivers.assign(1,*rtp1);
 
+    // On we go!
     gst_element_set_state(pipeline, GST_STATE_PLAYING);
     g_main_loop_run(loop);
     return 0;
